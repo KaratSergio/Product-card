7fe5fd6389df21e90ab08fdeaff1e32efb9837ca
Serhii Safonov <137483700+KaratSergio@users.noreply.github.com>
2024-10-24 18:04:03 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/product-card/',
+  base: '/Product-card/',
   plugins: [react()],
 });
